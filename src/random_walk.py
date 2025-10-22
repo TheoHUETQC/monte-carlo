5961cd13a5ce1ca3849b7a5209c45951d665b3ea
Théo HUET <theo6huet@gmail.com>
2025-10-22 20:17:05 +0200

    Update random_walk.py

--- src/random_walk.py
+++ src/random_walk.py
@@ -1,20 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 10**2
+N = 10**2 #nombre de simulation que l on fait pour avoir une moyenne correcte
 N_step = 10**3
 
-#init
-x = np.zeros(N_step)
 x_final = [] #tableau de resultat
 
 for i in range(N) :
+    x = np.zeros(N_step)
     for n in range(N_step) :
         step = np.random.choice([-1, 1])
         x[n] = x[n-1] + step
     x_final.append(x[n]) #on sauvegarde le resultat
-    print(x[n], x[n]*x[n])
+    plt.plot(np.linspace(0, N_step, N_step), x)
+    
 x_final = np.array(x_final)
+average = np.mean(x_final)
+variance = np.mean(x_final*x_final)
+print(average, " = <x>, doit etre egale a 0 ")
+print(variance, " = <x²>, doit etre egale a", N_step)
 
-print(np.mean(x_final), " = <x>, doit etre egale a 0 ")
-print(np.mean(x_final*x_final), " = <x²>, doit etre egale a", N)
+plt.title(f"Pour {N_step} step, on a  <x²> = {variance}, et <x> = {average}") #  (r"")
+
+plt.show()
